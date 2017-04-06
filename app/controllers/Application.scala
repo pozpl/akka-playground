7074a4f37e07961bd7be07385a73d5fcb2cd35a9
Pavel Pozdnyak <pozdpl@gmail.com>
2017-04-06 21:12:39 +1200

    Use new websocket handling API

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,8 @@ import javax.inject.{Inject, Singleton}
 import actors.ClientActor
 import akka.actor._
 import akka.stream.Materializer
-import play.api.Play.current
 import play.api.libs.json.JsValue
+import play.api.libs.streams.ActorFlow
 import play.api.mvc._
 import service.autorisation.AuthorizationServiceMemory
 import service.conversations.ConversationsServiceImplInMemory
@@ -30,9 +30,9 @@ class Application @Inject()(actorSystem: ActorSystem, eventBus: ChatEventBus,
   val chatService = actorSystem.actorOf(Props(new ChatService(eventBus, authService, conversationsService)))
 
   
-  def socket = WebSocket.acceptWithActor[JsValue, JsValue] {
+  def socket = WebSocket.accept[JsValue, JsValue] {
     (request: RequestHeader) => {
-      (out: ActorRef) => Props(new ClientActor(out, chatService, eventBus))
+      ActorFlow.actorRef((out:ActorRef )=> Props(new ClientActor(out, chatService, eventBus)))
     }
   }
 
