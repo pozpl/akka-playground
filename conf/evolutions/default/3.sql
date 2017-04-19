389096eb6ada3d52388517db760f2e5f272bfa02
Pavel Pozdnyak <pozdpl@gmail.com>
2017-04-19 22:59:26 +1200

    Fixed SQL

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -8,10 +8,11 @@ create table if not exists user_individual_subscription(
   `subscribed_user_id` varchar(100) NOT NULL,
   `position` INT(10) NOT NULL DEFAULT 0,
   CONSTRAINT `uis_user_id_sub_user_id_uc` UNIQUE (user_id, subscribed_user_id),
-  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES user(user_id) ON delete cascade,
-  CONSTRAINT user_id_fk FOREIGN KEY (subscribed_user_id) REFERENCES user(user_id) ON delete cascade
+  CONSTRAINT uis_user_id_fk FOREIGN KEY (user_id) REFERENCES `user`(user_id) ON delete cascade,
+  CONSTRAINT uis_subscribed_user_id_fk FOREIGN KEY (subscribed_user_id) REFERENCES `user`(user_id) ON delete cascade
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 # --- !Downs
 
 DROP TABLE user_individual_subscription;
+
