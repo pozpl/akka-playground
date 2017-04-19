06ab1a3593b3e635df623fd1e1eaa3b6d02a6257
Pavel Pozdnyak <pozdpl@gmail.com>
2017-04-19 22:53:41 +1200

    User to user individual subscription table

---
+++ conf/evolutions/default/3.sql
@@ -0,0 +1,17 @@
+# Users schema
+
+# --- !Ups
+
+create table if not exists user_individual_subscription(
+  `id` INT NOT NULL PRIMARY KEY,
+  `user_id` varchar(100) NOT NULL,
+  `subscribed_user_id` varchar(100) NOT NULL,
+  `position` INT(10) NOT NULL DEFAULT 0,
+  CONSTRAINT `uis_user_id_sub_user_id_uc` UNIQUE (user_id, subscribed_user_id),
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES user(user_id) ON delete cascade,
+  CONSTRAINT user_id_fk FOREIGN KEY (subscribed_user_id) REFERENCES user(user_id) ON delete cascade
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# --- !Downs
+
+DROP TABLE user_individual_subscription;
